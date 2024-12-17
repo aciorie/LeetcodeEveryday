35f995edd61c4cffbca42e1eb480afd90776d3a0
aciorie <scendrio@outlook.com>
2024-12-17 20:13:28 +0800

    No.101

---
+++ Hot_1st/101_Symmetric Tree.go
@@ -0,0 +1,43 @@
+package hot
+
+/*
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+
+
+Constraints:
+The number of nodes in the tree is in the range [1, 1000].
+-100 <= Node.val <= 100
+
+
+Follow up: Could you solve it both recursively and iteratively?
+*/
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return isMirror(root.Left, root.Right)
+}
+
+func isMirror(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+	if left.Val != right.Val {
+		return false
+	}
+
+	return isMirror(left.Left, right.Right) && isMirror(left.Right, right.Left)
+}
---
+++ Hot_1st/101_Symmetric Tree_test.go
@@ -0,0 +1,3 @@
+package hot
+
+//fine i don't know how to write this testcase so i'll give myself a break
