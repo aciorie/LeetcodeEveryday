d4b6a965c642ce5599c4c292068a88e5d66e9c2a
aciorie <scendrio@outlook.com>
2025-01-09 09:53:28 +0800

    No.169

---
+++ Hot_1st/169_Majority Element_test.go
@@ -0,0 +1,20 @@
+package hot
+
+import "testing"
+
+func TestMajorityElement(t *testing.T) {
+	inputs := []struct {
+		nums []int
+		want int
+	}{
+		{[]int{3, 2, 3}, 3},
+		{[]int{2, 2, 1, 1, 1, 2, 2}, 2},
+	}
+
+	for _, input := range inputs {
+		res := MajorityElement(input.nums)
+		if res != input.want {
+			t.Errorf("nums:%v,want:%v,got:%v", input.nums, input.want, res)
+		}
+	}
+}
