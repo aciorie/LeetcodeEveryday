655fd68d4dabf9c8ee8c0f3d1359f9b85896f3c5
aciorie <scendrio@outlook.com>
2025-01-12 09:20:55 +0800

    No.198

---
+++ Hot_1st/198_House Robber_test.go
@@ -0,0 +1,20 @@
+package hot
+
+import "testing"
+
+func TestRob(t *testing.T) {
+	inputs := []struct {
+		nums []int
+		want int
+	}{
+		{[]int{1, 2, 3, 1}, 4},
+		{[]int{2, 7, 9, 3, 1}, 12},
+	}
+
+	for _, input := range inputs {
+		res := Rob(input.nums)
+		if res != input.want {
+			t.Errorf("nums:%v,want:%v,got:%v", input.nums, input.want, res)
+		}
+	}
+}
