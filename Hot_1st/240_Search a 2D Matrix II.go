9870013c5840dde84e0c11a3fe76146d3671e4c4
aciorie <scendrio@outlook.com>
2025-01-22 16:41:58 +0800

    No.240

---
+++ Hot_1st/240_Search a 2D Matrix II.go
@@ -0,0 +1,81 @@
+package hot
+
+/*
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+
+
+Example 1:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+
+Example 2:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+
+
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= n, m <= 300
+-109 <= matrix[i][j] <= 109
+All the integers in each row are sorted in ascending order.
+All the integers in each column are sorted in ascending order.
+-109 <= target <= 109
+*/
+
+//got tle but dont know why
+func SearchMatrix_240(matrix [][]int, target int) bool {
+	m := len(matrix)
+	for i := 0; i < m; i++ {
+		if matrix[i][0] > target {
+			return false
+		}
+		if colBinarySearch(matrix[i], target) {
+			continue
+		} else {
+			return true
+		}
+	}
+	return false
+}
+
+func colBinarySearch(arr []int, target int) bool {
+	l, r := 0, len(arr)-1
+	for l <= r {
+		m := l + (r-l)/2
+		if arr[m] == target {
+			return true
+		} else if arr[m] > target {
+			r = m - 1
+		} else {
+			l = m
+		}
+	}
+	return false
+}
+
+func SearchMatrix_240_2(matrix [][]int, target int) bool {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+
+	m := len(matrix)
+	n := len(matrix[0])
+
+	// Start from top right
+	row, col := 0, n-1
+	for row < m && col >= 0 {
+		if matrix[row][col] == target {
+			return true
+		} else if matrix[row][col] > target {
+			col--
+		} else {
+			row++
+		}
+	}
+
+	return false
+}
