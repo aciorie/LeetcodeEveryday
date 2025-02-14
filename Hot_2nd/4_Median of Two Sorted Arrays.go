edb39e70f1923519a3096ccaae9bafcf36a5fcd0
aciorie <scendrio@outlook.com>
2025-02-14 09:05:25 +0800

    No.4

---
+++ Hot_2nd/4_Median of Two Sorted Arrays.go
@@ -0,0 +1,14 @@
+package hot2nd
+
+import "sort"
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	nums1 = append(nums1, nums2...)
+	sort.Ints(nums1)
+	m := len(nums1)
+	if m%2 == 1 {
+		return float64(nums1[m/2])
+	} else {
+		return float64(nums1[m/2]+nums1[m/2-1]) / 2.0
+	}
+}
