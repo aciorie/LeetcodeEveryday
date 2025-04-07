382cb34c77d63ab736274ccd4a52c552ac8b8ee1
aciorie <scendrio@outlook.com>
2025-04-07 09:14:21 +0800

    25.4.7

---
+++ Hot_2nd/84_Largest Rectangle in Histogram.go
@@ -0,0 +1,26 @@
+package hot2nd
+
+import "math"
+
+// TLE
+func largestRectangleArea(heights []int) int {
+	if len(heights) < 2 {
+		return heights[0]
+	}
+
+	res := 0
+	for i := 0; i < len(heights); i++ {
+		minHeight := math.MaxInt32
+		for j := i; j < len(heights); j++ {
+			// Fine minimum height
+			for k := i; k <= j; k++ {
+				if heights[k] < minHeight {
+					minHeight = heights[k]
+				}
+			}
+			// Now got minimum height
+			res = max(minHeight*(j-i+1), res)
+		}
+	}
+	return res
+}
